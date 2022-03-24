7278b3a0a148c21d2d2fad67643e5b435725b882
scottjohnson <scottwwj@gmail.com>
2022-03-24 15:44:12 -0500

    Add Directory component and category items

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,12 @@
+import './category-item.styles.scss'
+
+const CategoryItem = ({category: {title, imageUrl}}) =>
+  <div className={'category-container'}>
+    <div className={'background-image'} style={{backgroundImage: `url(${imageUrl}`}}/>
+    <div className={'category-body-container'}>
+      <h2>{title}</h2>
+      <p>Show Now</p>
+    </div>
+  </div>
+
+export default CategoryItem
