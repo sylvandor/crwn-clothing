bb21148185f708bf68d1b0c9e60a023587ac0fdb
scottjohnson <scottwwj@gmail.com>
2022-04-12 07:44:35 -0500

    Only log state changes when not in production

--- src/store/store.js
+++ src/store/store.js
@@ -12,7 +12,7 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-const middleWares = [logger];
+const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
 export const store = createStore(persistedReducer, undefined, composedEnhancers);
